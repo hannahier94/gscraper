e285d11704c0a73e694a3fbdc5c27e716104b147
hannahier94 <hannahier94@gmail.com>
2021-11-18 10:56:24 +0200

    removing csrf

--- dep.py
+++ dep.py
@@ -101,6 +101,6 @@ if __name__ == '__main__':
 
     port = os.environ.get('PORT')
     if port:
-        app.run(host='0.0.0.0', port=int(port))
+        app.run(host='0.0.0.0', port=int(port) ,debug=True)
     else:
-        app.run()
+        app.run(debug=True)
